d34b62450fccf1ff47f21a25fd38e21a64e005b9
ncchen99 <ncchen99@gmail.com>
2021-10-19 23:02:47 +0800

    😺

--- Vincent55/shortUrl/assets/js/custom.js
+++ Vincent55/shortUrl/assets/js/custom.js
@@ -101,11 +101,6 @@ function show_error_input($selector, error_message, original_message) {
 }
 
 function validURL(str) {
-    var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-        '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+    var pattern = new RegExp(/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/);
     return !!pattern.test(encodeURI(str));
 }
